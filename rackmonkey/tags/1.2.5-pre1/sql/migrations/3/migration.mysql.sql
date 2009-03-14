0a873d1bdca2aea0abf559eff7e904d3ffe77c44
Will Green <inbox@flux.org.uk>
2009-03-14 12:32:16 +0000

    Tagged for v1.2.5 Preview 1 prior to packaging and testing.

---
+++ rackmonkey/tags/1.2.5-pre1/sql/migrations/3/migration.mysql.sql
@@ -0,0 +1,6 @@
+-- ---------------------------------------------------------------------------
+-- RackMonkey - Know Your Racks - http://www.rackmonkey.org                 --
+-- Version 1.2.%BUILD%                                                      --
+-- (C)2004-2009 Will Green (wgreen at users.sourceforge.net)                --
+-- Update database schema to v3 for MySQL                                   --
+-- ---------------------------------------------------------------------------
