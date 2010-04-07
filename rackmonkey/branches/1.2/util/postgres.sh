95b3b68e83ced60c44f0b7038c4b147b3d74a96d
wgreen <wgreen@d21c1205-ac1a-0410-ae1a-9dc866783d85>
2010-04-07 18:11:17 +0000

    Updated database name to match convention used by trunk DB.

--- rackmonkey/branches/1.2/util/postgres.sh
+++ rackmonkey/branches/1.2/util/postgres.sh
@@ -1,6 +1,6 @@
-dropdb rackmonkey1_2
-createdb -O rackmonkey rackmonkey1_2
-psql -U rackmonkey rackmonkey1_2 < sql/schema/schema.postgres.sql 
-psql -U rackmonkey rackmonkey1_2 < sql/data/default_data.sql 
-psql -U rackmonkey rackmonkey1_2 < sql/data/sample_data.sql 
-psql -U rackmonkey rackmonkey1_2 < sql/data/test_data.sql
+dropdb rackmonkey-1_2
+createdb -O rackmonkey-1_2 rackmonkey-1_2
+psql -U rackmonkey-1_2 rackmonkey-1_2 < sql/schema/schema.postgres.sql 
+psql -U rackmonkey-1_2 rackmonkey-1_2 < sql/data/default_data.sql 
+psql -U rackmonkey-1_2 rackmonkey-1_2 < sql/data/sample_data.sql 
+psql -U rackmonkey-1_2 rackmonkey-1_2 < sql/data/test_data.sql
